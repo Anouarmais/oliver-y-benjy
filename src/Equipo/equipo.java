2436881f135e1b615025fb5f88f64e5748a97350
Administrador <maisanouar30@gmail.com>
2024-02-06 20:32:54 +0100

    hola

--- src/Equipo/equipo.java
+++ src/Equipo/equipo.java
@@ -9,4 +9,24 @@ public class equipo {
     public String nombreEquipo ;
     public List<Jugador> Jugadores;
 
+    public equipo(String nombreEquipo, List<Jugador> jugadores) {
+        this.nombreEquipo = nombreEquipo;
+        this.Jugadores = jugadores;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public List<Jugador> getJugadores() {
+        return Jugadores;
+    }
+
+    public void setJugadores(List<Jugador> jugadores) {
+        Jugadores = jugadores;
+    }
 }
